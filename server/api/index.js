562cb5531e523ebb867adddeb0b895dc5d727e06
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 17:57:20 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import hello from '../routes/hello.js'
 import cors from 'cors'
-// import analyseResume from '../routes/analyseResume.js'
+import analyseResume from '../routes/analyseResume.js'
 import interviewData from '../routes/interviewData.js'
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
-// app.use('/resume-content', analyseResume)
+
 app.use("/hii", hello)
 app.use('/interview-data', interviewData)
+app.use('/resume-content', analyseResume)
 app.get("/", (req, res) => res.send("welcome to Interro AI"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
