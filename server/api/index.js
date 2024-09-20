470e83f68eb5df6d05e60c8038d137d42f95dbc9
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 16:49:11 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import resume from '../routes/resume.js'
 const app = express();
 
+app.use('/resume', resume)
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
