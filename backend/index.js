11f1610902bdfae35a3c5928f1175e391a2c77b4
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 13:57:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get
 app.use('/resume-details', summarizeResume)
 app.use('/resume-content', analyseResume)
 app.use('/interview-data', interviewData)
-
+app.get('/test', (req, res) => {
+    res.status(200).send("Hello World");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> {
     console.log(`Server is running on ${PORT}`)
