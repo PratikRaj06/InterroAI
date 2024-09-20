72f4af9bf0c2bd86e099262fae549fe550b60e1a
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 17:15:00 +0530

    Delete server directory

--- server/routes/resume.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.get('/summarize', (req, res) => {
-    res.status(200).send("This is your resume")
-})
-
-export default router
