eaf5baa096502e4279166fed4b5370ce95cd8663
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 17:23:13 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
-
+import hello from '../routes/hello.js'
 import cors from 'cors'
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/hii", hello)
 app.get("/", (req, res) => res.send("welcome to Interro AI"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+export default app;
