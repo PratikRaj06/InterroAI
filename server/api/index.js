26de7b02381fa6675b879da7e513b1efb77eaa27
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 17:39:11 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import hello from '../routes/hello.js'
 import cors from 'cors'
+import analyseResume from '../routes/analyseResume.js'
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/resume-content', analyseResume)
 app.use("/hii", hello)
 app.get("/", (req, res) => res.send("welcome to Interro AI"));
 
