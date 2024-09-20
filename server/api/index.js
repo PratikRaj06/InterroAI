a637e961f4e1c0d796be8cde0b246df2f515ed6e
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 17:07:11 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import resume from '../routes/resume.js'
-import summarizeResume from '../routes/summarizeResume.js'
-import analyseResume from '../routes/analyseResume.js'
-import interviewData from '../routes/interviewData.js'
-import cors from 'cors';
+// import summarizeResume from '../routes/summarizeResume.js'
+// import analyseResume from '../routes/analyseResume.js'
+// import interviewData from '../routes/interviewData.js'
+// import cors from 'cors';
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
-app.use('/resume-details', summarizeResume)
-app.use('/resume-content', analyseResume)
-app.use('/interview-data', interviewData)
+// app.use('/resume-details', summarizeResume)
+// app.use('/resume-content', analyseResume)
+// app.use('/interview-data', interviewData)
 
 app.use('/resume', resume)
 app.get("/", (req, res) => res.send("Welcome to Interro AI"));
