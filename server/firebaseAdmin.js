df3cca10db75c455660db0642559013652fc94b1
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-20 17:36:22 +0530

    Create firebaseAdmin.js

---
+++ server/firebaseAdmin.js
@@ -0,0 +1,12 @@
+import admin from 'firebase-admin'
+import dotenv from 'dotenv'
+dotenv.config()
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+const auth = admin.auth()
+
+export { db, auth };
