ccdcd64e45eb8fd6e180dee5f9d0dad12182e110
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-21 14:59:54 +0530

    Update firebase.jsx

--- frontend/src/firebase.jsx
+++ frontend/src/firebase.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,10 +14,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
+const storage = getStorage(app);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider };
+export { auth, googleProvider, storage };
 
 
 
