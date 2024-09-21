9f911dcfa3c3bbd4111516e9a567160bdc4aaf6e
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-21 15:36:01 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import logoName from '../assets/images/logoName.webp'
+import { useNavigate } from 'react-router-dom'
 const Header = () => {
+    const navigate = useNavigate()
     return (
         <div className='w-full py-1 lg:px-10 px-5 bg-gradient-to-r from-blue-medium to-pink flex items-center justify-between'>
-            <img onClick={() => window.location.href='/home'} draggable={false} src={logoName} className='lg:h-6 h-4 my-2 cursor-pointer' alt="" />
+            <img onClick={() => navigate('/home')} draggable={false} src={logoName} className='lg:h-6 h-4 my-2 cursor-pointer' alt="" />
         </div>
     )
 }
