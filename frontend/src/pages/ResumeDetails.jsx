31094debbd9bf0bb63b6ab359d3597184df689bc
Pratik Raj <142412432+PratikRaj06@users.noreply.github.com>
2024-09-21 14:59:29 +0530

    Update ResumeDetails.jsx just now abhi

--- frontend/src/pages/ResumeDetails.jsx
+++ frontend/src/pages/ResumeDetails.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../contexts/auth'
 import Header from '../components/Header'
 import { PortraitContext } from '../contexts/portrait';
+import { storage } from '../firebase';
+import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 
 const ResumeDetails = () => {
     const { accessToken } = useContext(AuthContext)
@@ -12,11 +14,12 @@ const ResumeDetails = () => {
     const [processing, setProcessing] = useState(false)
     const [resumeResult, setResumeResult] = useState(null)
     const { isPortrait } = useContext(PortraitContext)
+
     const API_URL = import.meta.env.VITE_API_URL;
 
     const navigate = useNavigate()
-    useEffect(() => { 
-        if(resumeResult) navigate('/home/resume-ats-score/analysis-results', { state: { resumeData: resumeResult }})   
+    useEffect(() => {
+        if (resumeResult) navigate('/home/resume-ats-score/analysis-results', { state: { resumeData: resumeResult } })
     }, [resumeResult, navigate])
 
     const filePickerRef = useRef(null)
@@ -25,47 +28,50 @@ const ResumeDetails = () => {
         setError('');
 
         if (!file) {
-            return setError("Please select resume file")
+            return setError("Please select a resume file");
         }
 
         setProcessing(true);
 
         try {
-            const formData = new FormData()
-            formData.append('file', file)
-            formData.append('jobDescription', jobDescription)
+            const filename = file.name;
+            const fileRef = ref(storage, `uploads/${filename}`);
+            await uploadBytes(fileRef, file);
+            const fileUrl = await getDownloadURL(fileRef);
+
+            console.log(`File uploaded successfully! File URL: ${fileUrl}`);
 
             const response = await fetch(`${API_URL}/resume-content/analyse`, {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
                 },
-                body: formData,
+                body: JSON.stringify({ jobDescription, filename, fileUrl })
             });
-
-            const result = await response.json();
-
-            if (response.status === 500) { 
-                setError(result.message)
-                setProcessing(false)
+            const result = await response.json()
+            
+            await deleteObject(fileRef);
+            if (!response.ok) {
+                setError(result.message || "An error occurred");
             } else {
-                setResumeResult(result.resumeResult)
-                setProcessing(false);
+                setResumeResult(result.resumeResult);
             }
         } catch (error) {
+            console.error('Upload failed:', error);
+            setError(`Upload failed: ${error.message}`);
+        } finally {
             setProcessing(false);
-            setError(error.message);
         }
     };
-
 
 
     return (
         <>
             <div className='min-h-screen w-screen flex flex-col items-center'>
-                <Header/>
+                <Header />
                 <div className={`${processing && 'opacity-20'} w-full flex justify-center lg:p-10 p-5`}>
-                    <div className={`${isPortrait? 'md:w-11/2 w-full': 'lg:w-1/2 w-10/12'} bg-pink-light lg:p-5 md:p-3 p-1 rounded-lg shadow-lg flex flex-col items-center gap-3`}>
+                    <div className={`${isPortrait ? 'md:w-11/2 w-full' : 'lg:w-1/2 w-10/12'} bg-pink-light lg:p-5 md:p-3 p-1 rounded-lg shadow-lg flex flex-col items-center gap-3`}>
                         <h1 className='lg:text-4xl text-3xl font-bold text-purple text-center my-5'>ATS Score Checker</h1>
                         <label htmlFor="" className='w-11/12 flex flex-col items-start'>
                             <p className='text-md font-semibold text-blue-dark p-2'>Job Description</p>
@@ -77,7 +83,7 @@ const ResumeDetails = () => {
 
                         <label htmlFor="" className='w-11/12 flex flex-col items-start'>
                             <p className='text-md font-semibold text-blue-dark p-2'>Resume<span className='text-red'>*</span></p>
-                            <div className={`bg-[#fdf4ff] w-full py-2 px-3 rounded-lg border-2 border-pink border-opacity-25 flex ${isPortrait? 'flex-col': 'flex-row'} items-center justify-between gap-2`}>
+                            <div className={`bg-[#fdf4ff] w-full py-2 px-3 rounded-lg border-2 border-pink border-opacity-25 flex ${isPortrait ? 'flex-col' : 'flex-row'} items-center justify-between gap-2`}>
                                 <p className='p-1'>{file ? file.name : 'No file chosen'}</p>
                                 <input ref={filePickerRef} type="file" accept='application/pdf' onChange={(event) => {
                                     setFile(event.target.files[0])
